9fc09f187f72f091d3af18e896dfdb2794dd02ec
Cole <cajole@live.com>
2021-01-10 21:09:16 -0800

    first commit

---
+++ coen175_project/phase1/examples/test.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+
+char greeting[] = "hello world"; 
+
+int main (void ) {
+    /* he;lakj *************** askfasd f*/
+
+    string s = "hello /* world */ \" this is a quote within a quote \" ";
+}
+
+/*
+
+sadf asdf as
+
+asdf asdf 
